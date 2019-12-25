f267e00ff13e6373118164664240c3b1f03a5227
Gwamaka Charles <gwaxart19@gmail.com>
2019-12-25 11:59:54 +0300

    final

---
+++ build/precache-manifest.6d3457ba8f6f5e947205624e90a08812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175ab0dfba118362fb00048b0eae5aaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f6739d534ad0a9bf4b",
+    "url": "/static/css/main.447785a5.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "01f6739d534ad0a9bf4b",
+    "url": "/static/js/main.aeab2476.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
